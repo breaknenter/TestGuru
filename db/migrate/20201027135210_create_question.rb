2f984c7b1ba1f354cc425b5ad51688ea0d4b4795
Сергей <breaknenter@mail.ru>
2020-10-28 21:07:04 +0900

    Откат миграций

--- db/migrate/20201027135210_create_question.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestion < ActiveRecord::Migration[6.0]
-  def change
-    create_table :questions do |t|
-      t.string :text, null: false
-
-      add_reference :tests, :test, foreign_key: true
-    end
-  end
-end
