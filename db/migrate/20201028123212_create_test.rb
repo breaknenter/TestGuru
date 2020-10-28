982575f609ba6cf6cf328505f26483be0efd756b
Сергей <breaknenter@mail.ru>
2020-10-28 21:58:52 +0900

    Добавлены миграции

---
+++ db/migrate/20201028123212_create_test.rb
@@ -0,0 +1,9 @@
+class CreateTest < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, default: 0
+      t.references :category
+    end
+  end
+end
