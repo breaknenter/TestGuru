6a44322d66cb2ddd59a25b90eabb5be5620b23c7
Сергей <breaknenter@mail.ru>
2020-10-29 04:52:01 +0900

    Откат

--- db/migrate/20201028123401_create_question.rb
+++
@@ -1,8 +0,0 @@
-class CreateQuestion < ActiveRecord::Migration[6.0]
-  def change
-    create_table :questions do |t|
-      t.string :text, null: false
-      t.references :test
-    end
-  end
-end
--- db/migrate/20201028123512_create_answer.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswer < ActiveRecord::Migration[6.0]
-  def change
-    create_table :answers do |t|
-      t.string :text, null: false
-      t.boolean :correct, default: false
-      t.references :question
-    end
-  end
-end
