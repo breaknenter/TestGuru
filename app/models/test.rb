6efe0f8919029e856b53619323ab4e945e36488e
Сергей <breaknenter@mail.ru>
2020-11-11 22:57:13 +0900

    Добавлены семена и 2 метода

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category)
+    Test.joins("JOIN categories ON category_id = tests.category_id")
+      .where("categories.title = ?", category)
+      .order("tests.title DESC")
+      .pluck("tests.title")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  def list_of_tests(level)
+    User.joins("JOIN tests ON user_tests.user_id = user_tests.test_id")
+      .where("tests.level = ?", level)
+      .order("tests.title")
+      .pluck("tests.title")
+  end
 end
