d2540e8954c6389a1aaf535f3d2572df0020dba6
Сергей <breaknenter@mail.ru>
2020-11-14 02:01:26 +0900

    Изменён метод User#list_of_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def list_of_tests(level)
-    User.joins("JOIN tests ON user_tests.user_id = user_tests.test_id")
+    Test.joins("JOIN user_tests ON tests.id = user_tests.test_id")
       .where("tests.level = ?", level)
       .order("tests.title")
       .pluck("tests.title")
