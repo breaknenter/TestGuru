e36b7b92aeb5026a8e61f6df567c10befd8dc0a4
Сергей <breaknenter@mail.ru>
2020-11-23 23:42:04 +0900

    Добавлены связи между моделями

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,14 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: "User"
+  belongs_to :category
+  has_many   :questions
+  has_many   :user_tests
+  has_many   :users, through: :user_tests
+
   def self.tests_by_category(category)
     Test.joins("JOIN categories ON category_id = tests.category_id")
-      .where("categories.title = ?", category)
-      .order("tests.title DESC")
-      .pluck("tests.title")
+        .where("categories.title = ?", category)
+        .order("tests.title DESC")
+        .pluck("tests.title")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
   def list_of_tests(level)
     Test.joins("JOIN user_tests ON tests.id = user_tests.test_id")
-      .where("tests.level = ?", level)
-      .order("tests.title")
-      .pluck("tests.title")
+        .where("tests.level = ?", level)
+        .order("tests.title")
+        .pluck("tests.title")
   end
 end
