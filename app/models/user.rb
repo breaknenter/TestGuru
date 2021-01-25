52027ef672e7e0e54609d9c6519b22ccede34aa2
breaknenter <breaknenter@mail.ru>
2021-01-26 05:30:07 +0900

    Добавлена логика приложения

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_many :added_tests, class_name: "Test", foreign_key: "author_id"
-  has_many :user_tests
-  has_many :tests, through: :user_tests
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
-  scope :list_of_tests, -> (level) { joins(:tests).where("tests.level = ?", level)
-                                                  .order(:title) }
+  scope :list_of_tests, -> (level) { joins(:tests)
+  	                                   .where("tests.level = ?", level)
+                                       .order(:title) }
 
   validates :name,  presence: true, length: { in: 4..16 }
   validates :email, presence: true, uniqueness: true
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
