58fcbb9255b44ba855f9a1bfad5d221b7beecc1b
breaknenter <breaknenter@mail.ru>
2021-05-11 21:46:23 +0900

    Исправлено условие в экшене update

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -19,7 +19,7 @@ class TestPassagesController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed?
+    if @test_passage.completed? || !@test_passage.in_time?
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
